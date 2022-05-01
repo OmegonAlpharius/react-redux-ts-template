02d7fcd87773b0c2ba9a9bf0f61eb6b5f239eaed
OmegonAlpharius <tng.tarasenko@gmail.com>
2022-05-01 22:31:28 +0600

    add UserList

---
+++ src/components/UserList.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
+
+const UserList: React.FC = () => {
+	const {users,error,loading} = useTypedSelector(state=>state.user)
+
+
+	return <div>
+
+	</div> ;
+};
+
+export default UserList;
